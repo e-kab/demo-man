734aabe51392f173631d762e27c65e5b34955ee0
Ethan K <eak2236@columbia.edu>
2025-01-31 06:51:13 -0500

    fine-tuning player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5;
+    Rigidbody2D rb2d;
+    public KeyCode inputLeft = KeyCode.A;
+    public KeyCode inputRight = KeyCode.D;
+    public KeyCode inputUp = KeyCode.W;
+    public KeyCode inputDown = KeyCode.S;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float inputX = 0;
+        float inputY = 0;
+
+        if (Input.GetKey(inputUp))
+        {
+            inputY = 1;
+        }
+        if (Input.GetKey(inputDown))
+        {
+            inputY = -1;
+        }
+        if (Input.GetKey(inputLeft))
+        {
+            inputX = -1;
+        }
+        if (Input.GetKey(inputRight))
+        {
+            inputX = 1;
+        }
+
+        Vector2 direction = new Vector2(inputX, inputY);
+        if (direction.magnitude > 1)
+        {
+            direction.Normalize();
+        }
+        rb2d.linearVelocity = direction * speed;
     }
 }
