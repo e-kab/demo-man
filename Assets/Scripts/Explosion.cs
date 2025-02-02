7cadf720404dba2af1c1cb9daf3495e0d9088f79
Ethan K <eak2236@columbia.edu>
2025-02-02 13:43:29 -0500

    adding explosion frame functionality

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using System.Collections;
 
 public class Explosion : MonoBehaviour
 {
+
+    [Tooltip("The individual sprites of the animation")]
+    public Sprite[] frames;
+    [Tooltip("How fast does the animation play")]
+    public float framesPerSecond = 5;
+
+
+    SpriteRenderer spriteRenderer;
+    int currentFrameIndex = 0;
+    float frameTimer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        frameTimer = (1f / framesPerSecond);
+        currentFrameIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        frameTimer -= Time.deltaTime;
+
+        if (frameTimer <= 0)
+        {
+            currentFrameIndex++;
+            if (currentFrameIndex >= frames.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            frameTimer = (1f / framesPerSecond);
+            spriteRenderer.sprite = frames[currentFrameIndex];
+        }
     }
 }
