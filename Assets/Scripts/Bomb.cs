f645885cffda0e22e4cae108f806760bc9bf9b75
Ethan K <eak2236@columbia.edu>
2025-02-02 13:55:26 -0500

    adding explosion animation

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,18 +4,36 @@ public class Bomb : MonoBehaviour
 {
 
     public GameObject explosionPrefab;
+    [Tooltip("The individual sprites of the animation")]
+    public Sprite[] frames;
+    [Tooltip("How fast does the animation play")]
+    public float framesPerSecond = 3;
 
+
+    SpriteRenderer spriteRenderer;
+    int currentFrameIndex = 0;
+    float frameTimer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        frameTimer = (1f / framesPerSecond);
+        currentFrameIndex = 0;
         Invoke("Explode", 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        frameTimer -= Time.deltaTime;
+
+        if (frameTimer <= 0)
+        {
+            currentFrameIndex++;
+            frameTimer = (1f / framesPerSecond);
+            spriteRenderer.sprite = frames[currentFrameIndex];
+        }
     }
 
     void Explode()
