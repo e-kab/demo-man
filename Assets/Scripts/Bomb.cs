3b42a13d7addef4e8e77f445b833104924ada072
Ethan K <eak2236@columbia.edu>
2025-02-02 13:48:45 -0500

    making bomb explode after 2 seconds

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -5,10 +5,11 @@ public class Bomb : MonoBehaviour
 
     public GameObject explosionPrefab;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Invoke("Explode", 2f);
     }
 
     // Update is called once per frame
